1a57870f7d984ce4c48b45a44a1fafdcb404c1fa
Marlon Joel Yaguana Manzano <marlonjoelyaguana@gmail.com>
2026-08-20 19:00:52 -0500

    Add files via upload

---
+++ Ejercicios_En_Clase/promedio/Codificación/Promedio3Notas.java
@@ -0,0 +1,31 @@
+package promedio3notas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Promedio3Notas {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        double n1, n2, n3, promedio;
+        
+        System.out.print("Ingrese la Nota 1: ");
+        n1 = scan.nextDouble();    
+        System.out.print("Ingrese la Nota 2: ");
+        n2 = scan.nextDouble();
+        System.out.print("Ingrese la Nota 3: ");
+        n3 = scan.nextDouble();
+        
+        promedio = (n1+n2+n3)/3;
+        
+        System.out.println("Primera nota ingresada: "+n1);
+        System.out.println("Segunda nota ingresada: "+n2);
+        System.out.println("Tercera nota ingresada: "+n3);
+        System.out.println("Su promedio es: "+promedio);
+    }
+    
+}
